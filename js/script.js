f8b5477e1a645819c9d8f16e1dc6218e144a50cc
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-05 05:28:53 +0300

    Added functionality for fetching and displaying random meal

--- js/script.js
+++ js/script.js
@@ -53,3 +53,42 @@ function handleSearch() {
             console.error(err)
         })
 }
+
+function showRandom(){
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        const mealsArray = data.meals
+        let randomDishContainer = document.querySelector('.random')
+        randomDishContainer.innerText = ''
+
+        mealsArray.forEach(meal => {
+            const dish = document.createElement('div')
+            dish.className = 'dish'
+
+            const thumbnail = document.createElement('img')
+            thumbnail.className = 'meal-thumbnail'
+            thumbnail.src = meal.strMealThumb
+            thumbnail.alt = meal.strMealThumb
+
+            const mealName = document.createElement('p')
+            mealName.className = 'meal-name'
+            mealName.innerText = meal.strMeal
+
+            const mealOrigin = document.createElement('p')
+            mealOrigin.className = 'meal-origin'
+            mealOrigin.innerText = meal.strArea
+
+            const recipeLink = document.createElement('p')
+            recipeLink.className = 'meal-recipe'
+            recipeLink.innerText = 'Link to recipe'
+
+            dish.appendChild(thumbnail)
+            dish.appendChild(mealName)
+            dish.appendChild(mealOrigin)
+            dish.appendChild(recipeLink)
+
+            randomDishContainer.appendChild(dish)
+        })
+    })
+}
